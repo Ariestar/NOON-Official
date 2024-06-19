ae93e258dbd6919c3fb8e0a7d32ad4d53e89c7cd
Ariestar <1941137088@qq.com>
2024-06-20 00:45:35 +0800

    homepage background

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const bg = ref<HTMLElement>()
+function bgmove(e: MouseEvent) {
+  const w = document.body.clientWidth
+  const h = document.body.clientHeight
+  
+  bg.value!.style.backgroundPositionX = 0.05 * (e.x - w / 2) + 'px'
+  bg.value!.style.backgroundPositionY = 0.02 * (e.y - h / 2) + 'px'
+}
+</script>
+
+<template>
+  <main @mousemove="bgmove" ref="bg"></main>
+</template>
+
+<style>
+main {
+  height: 100vh;
+  background: url(../assets/bg.png) no-repeat center/80%;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
